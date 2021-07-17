f11207e02fa1bc0c631f6f5705b49591d10ded72
absurpone <absurppone@gmail.com>
2021-07-17 14:23:16 +0900

    fizzbuzz test

--- test/fizzbuzz_test.rb
+++ test/fizzbuzz_test.rb
@@ -1,16 +1,5 @@
-def fizz_buzz(num)
-  if num % 15 == 0
-    'Fizz Buzz'
-  elsif num % 3 == 0
-    'Fizz'
-  elsif num % 5 == 0
-    'Buzz'
-  else
-    num.to_s
-  end
-end
-
 require 'minitest/autorun'
+require './lib/fizzbuzz'
 
 class FizzBuzzTest < Minitest::Test
   def test_fizzbuzz
